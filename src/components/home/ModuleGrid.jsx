98c2b194c502c1334c83b77f2573540b0c1def64
GeorgeBalu <egorbalyxin@mail.ru>
2026-08-07 14:43:39 +0500

    Новые переливы цвета модулей на главной

--- src/components/home/ModuleGrid.jsx
+++ src/components/home/ModuleGrid.jsx
@@ -75,6 +75,7 @@ export default function ModuleGrid() {
             <span className="module-grid__surface-texture" />
             <span className="module-grid__surface-beam module-grid__surface-beam--a" />
             <span className="module-grid__surface-beam module-grid__surface-beam--b" />
+            <span className="module-grid__surface-beam module-grid__surface-beam--c" />
           </span>
 
           <span className="module-grid__icon" aria-hidden="true">
