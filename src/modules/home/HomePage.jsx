8ad0b826b53b4d2df5e98ca733fcca124373e8a1
GeorgeBalu <egorbalyxin@mail.ru>
2026-08-14 23:15:30 +0500

    правим код

--- src/modules/home/HomePage.jsx
+++ src/modules/home/HomePage.jsx
@@ -1,6 +1,7 @@
 import HomeHeader from "@/modules/home/components/HomeHeader";
 import ModuleGrid from "@/modules/home/components/ModuleGrid";
 import NotificationSection from "@/modules/home/components/NotificationSection";
+import PageScroll from "@/components/app/PageScroll";
 
 import "./HomePage.css";
 
@@ -12,10 +13,12 @@ const mockUser = {
 
 export default function HomePage() {
   return (
-    <main className="home-page">
-      <HomeHeader {...mockUser} />
-      <NotificationSection />
-      <ModuleGrid />
-    </main>
+    <PageScroll>
+      <main className="home-page">
+        <HomeHeader {...mockUser} />
+        <NotificationSection />
+        <ModuleGrid />
+      </main>
+    </PageScroll>
   );
 }
