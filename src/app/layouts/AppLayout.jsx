80ee8020d4db825fdca24783758a39d5e660a23e
GeorgeBalu <egorbalyxin@mail.ru>
2026-08-16 14:19:28 +0500

    Начинаем переезд размера экарнов как на лендинге

--- src/app/layouts/AppLayout.jsx
+++ src/app/layouts/AppLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 import BottomTabBar from "@/components/app/BottomTabBar";
 
+import "./AppLayout.css";
+
 export default function AppLayout() {
   return (
     <>
-      <div className="app">
-        <div className="page">
+      <div className="app-layout">
+        <div className="app-layout__content">
           <Outlet />
         </div>
       </div>
