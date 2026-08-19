333cf28cd5c0d9990a54399fbe5995436a72c718
GeorgeBalu <egorbalyxin@mail.ru>
2026-08-19 17:47:34 +0500

    Сделали чтоб при окрытии всегда был верх страницы и вернули сей ареу на лендинг

--- src/app/layouts/AppLayout.jsx
+++ src/app/layouts/AppLayout.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
-
 import BottomTabBar from "@/components/app/BottomTabBar";
+import ScrollToTop from "@/components/common/ScrollToTop";
 
 import "./AppLayout.css";
 
 export default function AppLayout() {
   return (
     <>
+      <ScrollToTop />
+
       <div className="app-layout">
         <div className="app-layout__content">
           <Outlet />
--- src/app/layouts/LandingLayout.jsx
+++ src/app/layouts/LandingLayout.jsx
@@ -1,12 +1,18 @@
 import LandingPage from "@/pages/landing/LandingPage";
+import ScrollToTop from "@/components/common/ScrollToTop";
+
 import "./LandingLayout.css";
 
 export default function LandingLayout() {
   return (
-    <div className="landing-shell">
-      <div className="landing-page">
-        <LandingPage />
+    <>
+      <ScrollToTop />
+
+      <div className="landing-shell">
+        <div className="landing-page">
+          <LandingPage />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
