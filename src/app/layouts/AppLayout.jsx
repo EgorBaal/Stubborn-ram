fd02ca453665e4bc6e9f6c1a4968baec75797078
GeorgeBalu <egorbalyxin@mail.ru>
2026-08-20 16:07:36 +0500

    Архитектура: добавлена поддержка статических страниц без app-scroll

--- src/app/layouts/AppLayout.jsx
+++ src/app/layouts/AppLayout.jsx
@@ -1,19 +1,36 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import BottomTabBar from "@/components/app/BottomTabBar";
 import ScrollToTop from "@/components/common/ScrollToTop";
 
 import "./AppLayout.css";
 
+// Страницы, которые всегда помещаются на один экран.
+// Для них не используется app-scroll и резерв под TabBar.
+const STATIC_PAGES = ["/app/home"];
+
 export default function AppLayout() {
+  const location = useLocation();
+
+  const isStaticPage = STATIC_PAGES.includes(location.pathname);
+
   return (
     <>
       <ScrollToTop />
 
       <div className="app-shell">
         <div className="app-page">
-          <div className="app-scroll">
+          {/* 
+            Статические страницы рендерятся напрямую внутри app-page.
+            Все остальные страницы оборачиваются в app-scroll
+            и получают стандартную систему прокрутки.
+          */}
+          {isStaticPage ? (
             <Outlet />
-          </div>
+          ) : (
+            <div className="app-scroll">
+              <Outlet />
+            </div>
+          )}
         </div>
 
         <div className="app-tabbar">
